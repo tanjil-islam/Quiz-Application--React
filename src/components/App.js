8031d3438c93eec7dfacf0f1f0a5ddb26b6d7248
tanjil-islam <tanjil.islam040820@gmail.com>
2026-06-12 17:36:12 +0600

    Initial commit: Setup layout, navbar, and homepage UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
-
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello World</div>
-    </div>
+    <Layout>
+      <Home />
+    </Layout>
+    
   );
 }
 
