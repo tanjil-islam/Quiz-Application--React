d994008e67cfacc4e99ec5ae385b3c9b307274bf
tanjil-islam <tanjil.islam040820@gmail.com>
2026-06-14 23:30:56 +0600

    Implement dual-layer routing defense using PublicRoute and PrivateRoute

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom' // 1. Changed Switch to Routes
+import { AuthProvider } from '../contexts/AuthContext'
 import '../styles/App.css'
 import Layout from './Layout'
 import Home from './pages/Home'
@@ -6,21 +7,28 @@ import Login from './pages/Login'
 import Quiz from './pages/Quiz'
 import Result from './pages/Result'
 import Signup from './pages/Signup'
-import { AuthProvider } from '../contexts/AuthContext'
+import PrivateRoute from './PrivateRoute'
+import PublicRoute from './PublicRoute'
 
 function App() {
   return (
     <Router>
-      <AuthProvider>  
-      <Layout>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/quiz' element={<Quiz />} />
-          <Route path='/result' element={<Result />} />
-        </Routes>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<Home />} />
+
+            <Route element={<PublicRoute />}>
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/login' element={<Login />} />
+            </Route>
+
+            <Route element={<PrivateRoute />}>
+              <Route path='/quiz' element={<Quiz />} />
+              <Route path='/result' element={<Result />} />
+            </Route>
+          </Routes>
+        </Layout>
       </AuthProvider>
     </Router>
   )
--- src/components/Video.js
+++ src/components/Video.js
@@ -3,7 +3,7 @@ import classes from '../styles/Video.module.css'
 
 export default function Video() {
   return (
-    <a href='quiz.html'>
+    
       <div className={classes.video}>
         <img src={image} alt='' />
         <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
@@ -12,6 +12,7 @@ export default function Video() {
           <p>Score : Not taken yet</p>
         </div>
       </div>
-    </a>
+    
   )
 }
+
